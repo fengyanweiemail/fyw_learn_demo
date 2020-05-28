eff1e2b2f17a8a32e76a5213041ee16d01d9d999
WUNAN <634854235@qq.com>
2020-05-28 10:50:52 +0800

    学习笔记

---
+++ thread_demo/src/main/java/com/thread/ThreadApplication.java
@@ -0,0 +1,14 @@
+package com.thread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by fyw on 2020/5/28.
+ */
+@SpringBootApplication
+public class ThreadApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ThreadApplication.class,args);
+    }
+}
